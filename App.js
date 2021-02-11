4ba0c4023fe3de343f6a90055e01661a693027fd
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2021-02-11 10:29:33 -0300

    🟢 Connect react redux with store

--- App.js
+++ App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import {Provider} from 'react-redux';
+import {store} from './store/store';
+
 import Movies from './pages/Movies';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Movies">
-        <Stack.Screen name="Movies" component={Movies} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Movies">
+          <Stack.Screen name="Movies" component={Movies} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
