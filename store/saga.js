974a10b5f8e108212ae6abcaa9432f5275b04eb1
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2021-02-12 13:27:56 -0300

    add video details on saga and redux

--- store/saga.js
+++ store/saga.js
@@ -1,6 +1,8 @@
 import {fork} from 'redux-saga/effects';
 import {searchVideoWatcher} from './VideoStore/VideoStore.saga';
+import {searchVideoDetailsWatcher} from './VideoDetails/VideoDetails.saga';
 
 export function* rootSaga() {
   yield fork(searchVideoWatcher);
+  yield fork(searchVideoDetailsWatcher);
 }
--- store/store.js
+++ store/store.js
@@ -3,12 +3,14 @@ import createSagaMiddleware from 'redux-saga';
 import {rootSaga} from './saga';
 
 import {MoviesReducer} from './VideoStore/VideoStore.reducer';
+import {VideoDetailsReducer} from './VideoDetails/VideoDetails.reducer';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = createStore(
   combineReducers({
     videos: MoviesReducer,
+    video_details: VideoDetailsReducer,
   }),
   applyMiddleware(sagaMiddleware),
 );
