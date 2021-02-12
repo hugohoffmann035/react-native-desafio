59352ce74f00b5b32638ccf79a4c2b35928f2c2e
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2021-02-12 01:06:26 -0300

    improved field search

--- Components/Input/styles.js
+++ Components/Input/styles.js
@@ -1,18 +1,14 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  margin-top: 2;
-  margin-bottom: 2;
-  margin-left: 2;
-  margin-right: 2;
+  margin: 2px;
 `;
 
 export const Input = styled.TextInput`
   background-color: #d1d1d1;
-  height: 56px;
-  padding-left: 15px;
-  padding-right: 15px;
   border-radius: 6px;
+  height: 56px;
+  padding: 0px 15px;
 
   color: #333;
   font-size: 16px;
